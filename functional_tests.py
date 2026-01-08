93d9062fd9ae9245826ed2a9054e048dd4582209
cracedotdev <mail@crace.dev>
2026-01-08 11:44:48 -0500

    first ft specced out in comments, now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,33 @@
+import unittest
+
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert "Congratulations" in browser.title
-print("OK")
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_todo_list(self):
+        # user wants to use the Todo app
+        self.browser.get("http://localhost:8000")
+
+        # user sees the title
+        self.assertIn("To-Do", self.browser.title)
+
+        # user sees a box to enter todos
+        self.fail("Finish the test!")
+        # user enters "buy milk" in the box
+
+        # user sees page refresh, list contains "buy milk"
+
+        # user enters "drink milk" in the box
+
+        # user sees page refresh, list contains both todo items"
+
+        # user quits.
+
+if __name__ == "__main__":
+    unittest.main()
