712d33b1048acd589bbd3214cec976d0e2fe61f7
cracedotdev <mail@crace.dev>
2026-01-09 12:23:17 -0500

    func. test now checks we can input todo item

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,9 @@
+import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,16 +17,29 @@ class NewVisitorTest(unittest.TestCase):
         # user wants to use the Todo app
         self.browser.get("http://localhost:8000")
 
-        # user sees the title
+        # user notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("To-Do", header_text)
 
-        # user sees a box to enter todos
-        self.fail("Finish the test!")
+        # user is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(
+            inputbox.get_attribute("placeholder"), "Enter a to-do item"
+        )
         # user enters "buy milk" in the box
+        inputbox.send_keys("buy milk")
 
         # user sees page refresh, list contains "buy milk"
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertTrue(any(row.text == "1: buy milk" for row in rows))
 
         # user enters "drink milk" in the box
+        self.fail("Finish the test!")
 
         # user sees page refresh, list contains both todo items"
 
