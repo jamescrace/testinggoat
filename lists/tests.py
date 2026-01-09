cea93a84336fc7484d884814c16ee4fc2b501989
cracedotdev <mail@crace.dev>
2026-01-09 10:18:14 -0500

    first unit test/view function

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,14 @@
+from django.http import HttpRequest
 from django.test import TestCase
+
+from lists.views import home_page
 
 
 class SmokeTest(TestCase):
-    def test_bad_math(self):
-        self.assertEqual(2 + 2, 5)
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.startswith("<html>"))
+        self.assertTrue(html.endswith("</html>"))
