4214d162939fb90bbb06e06f478797ded5841b84
cracedotdev <mail@crace.dev>
2026-02-27 12:31:59 -0500

    feat: adjust new list view to do model validation

--- src/lists/tests/test_models.py
+++ src/lists/tests/test_models.py
@@ -1,5 +1,10 @@
+from ansible_collections.hitachivantara.vspone_block.plugins.module_utils.common.hv_exceptions import \
+    ValidationException
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from django.test import TestCase
 from lists.models import Item, List
+
 
 class ListItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
@@ -28,3 +33,20 @@ class ListItemModelTest(TestCase):
         self.assertEqual(first_saved_item.list, my_list)
         self.assertEqual(second_saved_item.text, "Item the second")
         self.assertEqual(second_saved_item.list, my_list)
+
+    def test_cannot_save_null_list_items(self):
+        mylist = List.objects.create()
+        item = Item(list=mylist, text=None)
+        with self.assertRaises(IntegrityError):
+            item.save()
+
+    def test_cannot_save_empty_list_items(self):
+        mylist = List.objects.create()
+        item = Item(list=mylist, text="")
+        with self.assertRaises(ValidationError):
+            item.full_clean()
+
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post("/lists/new", data={"item_text": ""})
+        self.assertEqual(Item.objects.count(), 0)
+        self.assertEqual(List.objects.count(), 0)
