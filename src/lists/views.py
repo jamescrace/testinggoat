e5df8a266f98e1f75fa7902969ffcc70771acbf9
cracedotdev <mail@crace.dev>
2026-03-04 11:48:05 -0500

    feat: enforce model validation in list view

--- src/lists/views.py
+++ src/lists/views.py
@@ -13,10 +13,16 @@ def home_page(request):
 
 def view_list(request, list_id):
     our_list = List.objects.get(id=list_id)
+    error = None
+
     if request.method == "POST":
-        Item.objects.create(text=request.POST['item_text'], list=our_list)
-        return redirect(f"/lists/{our_list.id}/")
-    return render(request, "list.html", {"list": our_list})
+        try:
+            item = Item(text=request.POST["item_text"], list=our_list)
+            item.full_clean()
+            item.save()
+        except ValidationError:
+            error = "You can't have an empty list item"
+    return render(request, "list.html", {"list": our_list, "error": error})
 
 
 def new_list(request):
@@ -30,5 +36,3 @@ def new_list(request):
         error = "You can't have an empty list item"
         return render(request, "home.html", {"error": error})
     return redirect(f"/lists/{nulist.id}/")
-
-
