a4da9723ace5687e009766a9d36770cfaaf80ce0
cracedotdev <mail@crace.dev>
2026-03-07 10:35:36 -0500

    feat: ft for dupe items

--- src/functional_tests/test_list_item_validation.py
+++ src/functional_tests/test_list_item_validation.py
@@ -48,6 +48,22 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("2: Make tea")
 
+    def test_cannot_add_duplicate_items(self):
+        # edith goes to the home page and starts a new list
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys("Buy wellies")
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy wellies")
 
-if __name__ == "__main__":
-    unittest.main()
+        # she accidentally enters a duplicate item
+        self.get_item_input_box().send_keys("Buy wellies")
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # she sees an error message
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element(By.CSS_SELECTOR, ".invalid-feedback").text,
+                "You've already got that on your list!"
+            )
+        )
+
